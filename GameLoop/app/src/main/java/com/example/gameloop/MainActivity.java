7afc389d34f41b40ca5da928625220f3794e5109
masnm <nomanahmmed0@gmail.com>
2020-11-25 08:03:03 +0600

    Added game loop in in gameloop project

---
+++ GameLoop/app/src/main/java/com/example/gameloop/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.gameloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        window.setFlags (
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        //setContentView(R.layout.activity_main);
+        // set the content view to game class
+        setContentView(new Game(this));
+
+    }
+}
